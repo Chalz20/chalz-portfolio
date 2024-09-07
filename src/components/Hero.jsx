88ead9f9abf10fc9efd080db56353bd4605b2f9c
Chalz20 <clkadete@gmail.com>
2024-09-07 12:02:16 +0300

    initializing

---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from "styled-components"
+
+const Container = styled.div`
+   padding: 0px;
+   margin: 0px;
+   height: 100vh;
+   background-color: blue;
+`
+
+export const Hero = () => {
+  return (
+    <Container>
+      Hero
+    </Container>
+    
+  )
+}
+
+export default Hero
